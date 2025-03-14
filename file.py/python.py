e41525345b0c0670933090e82e9152e4f42823a4
Samuel Nugusu <samuelnugusu12@gmail.com>
2025-03-14 23:01:08 +0300

    some folder changed

--- file.py/python.py
+++
@@ -1,7 +0,0 @@
-f = open("demofile3.txt", "w")
-f.write("Woops! I have deleted the content!")
-f.close()
-
-#open and read the file after the overwriting:
-f = open("demofile3.txt", "r")
-print(f.read())
--- index.php
+++ index.php
@@ -1,251 +1,109 @@
 <?php
-/**
- * Online-Job-Portal - A web application built on PHP HTML & javascript
-Copyright (C) 2025 
-
-
- */
+session_start();
+require_once 'includes/db.php';
 ?>
+
 <!DOCTYPE HTML>
 <html>
 <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Job Portal - Find Your Dream Job</title>
+    <link href="css/common.css" rel="stylesheet">
+    <link href="css/styles.css" rel="stylesheet">
+    <link href="css/future.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700&display=swap" rel="stylesheet">
+</head>
+<body>
+    <!-- Navbar -->
+    <nav class="navbar" id="insidenav">
+        <div class="nav-container">
+            <a href="index.php" class="nav-brand">Job Portal</a>
+            <button class="nav-toggle">☰</button>
+            <ul class="nav-links">
+                <li><a href="#main">Home</a></li>
+                <li><a href="search.php">Search Jobs</a></li>
+                <?php if (isset($_SESSION['employer_id'])): ?>
+                    <li><a href="post-job.php">Post a Job</a></li>
+                    <li><a href="employer-dashboard.php">My Jobs</a></li>
+                <?php endif; ?>
+                <?php if (isset($_SESSION['seeker_id'])): ?>
+                    <li><a href="jobseeker-dashboard.php">My Applications</a></li>
+                <?php endif; ?>
+                <li><a href="contact.php#contact">Contact Us</a></li>
+            </ul>
+            <ul class="nav-links right">
+                <?php if (!isset($_SESSION['seeker_id']) && !isset($_SESSION['employer_id'])): ?>
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle">Register</a>
+                        <ul class="dropdown-menu">
+                            <li><a href="jobseeker-register.php">Job Seeker</a></li>
+                            <li class="divider"></li>
+                            <li><a href="employer-register.php">Employer</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="login.php">Login</a></li>
+                <?php else: ?>
+                    <li><a href="logout.php">Logout</a></li>
+                <?php endif; ?>
+            </ul>
+        </div>
+    </nav>
 
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Hero/Search Section -->
+    <section id="main" class="search-section">
+        <div class="search-jumbotron">
+            <h1>Find Your Dream Job</h1>
+            <p>Explore thousands of job opportunities from top companies worldwide.</p>
+            <form class="search-form" action="search.php" method="GET">
+                <input type="text" class="input-field" placeholder="Enter job title or location" name="keyword">
+                <button type="submit" class="btn search-btn">Search Jobs</button>
+            </form>
+        </div>
+    </section>
 
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-      
-        <title> Job Portal </title>
-        <script type="application/javascript">
-            $(document).ready(function(){
-                // Add smooth scrolling to all links in navbar + footer link
-                $(".navbar a, footer a[href='#insidenav']").on('click', function(event) {
+    <!-- Secondary Call-to-Action -->
+    <section class="cta-section">
+        <p>Are you an employer looking to hire top talent?</p>
+        <a href="post-job.php" class="cta-btn">Post a Job Now</a>
+    </section>
 
-                    // Prevent default anchor click behavior
-                    event.preventDefault();
+    <!-- Include Future Jobs Section -->
+    <section class="future-jobs-section">
+        <?php include 'future-jobs.php'; ?>
+    </section>
 
-                    // Store hash
-                    var hash = this.hash;
+    <!-- Footer -->
+    <footer class="footer">
+        <a href="#insidenav" title="To Top">↑</a>
+        <p>Job Portal © 2025</p>
+    </footer>
 
-                    // Using jQuery's animate() method to add smooth page scroll
-                    // The optional number (900) specifies the number of milliseconds it takes to scroll to the specified area
-                    
-                    $('html, body').animate({
-                        scrollTop: $(hash).offset().top
-                    }, 900, function(){
+    <script src="js/scripts.js"></script>
+    <script>
+        // Toggle mobile menu
+        document.addEventListener('DOMContentLoaded', function() {
+            const navToggle = document.querySelector('.nav-toggle');
+            const navLinks = document.querySelector('.nav-links');
 
-                        // Add hash (#) to URL when done scrolling (default click behavior)
-                        window.location.hash = hash;
-                    });
+            navToggle.addEventListener('click', function() {
+                navLinks.classList.toggle('active');
+            });
+
+            // Smooth scroll for "To Top" link
+            document.querySelector('.footer a').addEventListener('click', function(e) {
+                e.preventDefault();
+                window.scrollTo({
+                    top: 0,
+                    behavior: 'smooth'
                 });
-                $(window).scroll(function() {
-                    $(".slideanim").each(function(){
-                        var pos = $(this).offset().top;
+            });
 
-                        var winTop = $(window).scrollTop();
-                        if (pos < winTop + 600) {
-                            $(this).addClass("slide");
-                        }
-                    });
-                });
-            })
-        </script>
-    </head>
-        
-<nav class="navbar" id="insidenav">
-  <div class="container-fluid">
-      <div class="navbar-header">
-          <a class="navbar-brand" href="#">Job Portal</a>
-      </div>
-
-    <ul class="nav navbar-nav">
-      <li class="active"><a data-toggle="tab" href="#main1">Home</a></li>
-        <li><a data-toggle="tab" href="#recent"">Recent Jobs</a></li>
-      <li><a data-toggle="tab" href="#jobseeker">Job Seeker</a></li>
-      <li><a data-toggle="tab" href="#">Employer</a></li>
-      <li><a data-toggle="tab" href="#contact">Contact Us</a></li>
-    </ul>
-
-    <ul class="nav navbar-nav navbar-right">
-                <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">
-                        <span class="glyphicon glyphicon-user"></span> Register <span class="caret"></span></a>
-                    <ul class="dropdown-menu">
-                        <li><a href="jobseeker/register_user.php">Jobseeker</a></li>
-                        <li role="separator" class="divider"></li>
-                        <li><a href="employer/register_emp.php">Employer</a></li>
-                    </ul>
-                </li>
-                <li><a href="login.php"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
-    </ul>
-  </div>
-</nav>
-    <!--- -------------------------------------------------------------------------------------------------- -->
-    <body id="indexbody" data-spy="scroll" data-target=".navbar" data-offset="60">
-<div class="bmsTop">
-    <ul>
-        <li style="font-size: 15px; font-weight: bold">Top Recruiters:</li>
-        <li><a href="#" target="_blank">
-                <img src="images/1.gif" border="0">
-            </a></li>
-        <li><a href="#" target="_blank">
-                <img src="images/2.gif" border="0">
-            </a></li>
-        <li><a href="#" target="_blank">
-                <img src="images/3.gif" border="0"></a>
-        </li>
-        <li><a href="#" target="_blank">
-                <img src="images/4.gif" border="0"></a></li>
-        <li><a href="#" target="_blank">
-                <img src="images/5.gif" border="0"></a>
-        </li>
-    </ul>
-</div>
-
-<div class="container-fluid" id="main1"> <!-- jumbotron fluid -->
-<div class="jumbotron text-center" id="searchjum">
-<h1>Job Portal</h1>
-    <p>Search for Jobs</p>
-    <form class="form-inline" id="homesearch">
-        <input type="text" class="form-control" size="50" placeholder="Enter your search keyword" name="keyword" id="keyword">
-        <button type="button" onclick="search()" class="btn btn-lg " style="color: black"><span class="glyphicon glyphicon-search"></span> Search</button>
-    </form>
-</div>
-</div> <!-- jumbotron -->
-
-<div class="container" id="subcontent" style="background: transparent">
-    <!-- div for search contents -->
-</div>
-<div class="page-header" style="background:#1abc9c"></div>
-<div class="container-fluid" style="background: transparent">
-    <div class="text-center">
-        <h2>Register</h2>
-        <h4>Register in this website for a better experience</h4>
-    </div>
-    <div class="row">
-        <div class="col-sm-4">
-            <div class="panel panel-default text-center">
-                <div class="panel-heading">
-                    <h1>Employers</h1>
-                </div>
-                <div class="panel-body">
-                    <p style="font-size: 16px">Register today and post a job in easy steps and start receiving applications the same day.
-                        Find the right candidates easily and quickly through our Search feature.</p>
-                </div>
-                <div class="panel-footer">
-                    <h3>$0</h3>
-                   <a href="employer/register_emp.php" style="color: inherit"> <button class="btn btn-lg">Sign Up</button></a>
-                </div>
-            </div>
-        </div>
-        <div class="col-sm-4">
-            <div class="panel panel-default text-center">
-                <div class="panel-heading">
-                    <h1>Job Seekers</h1>
-                </div>
-                <div class="panel-body">
-                    <p style="font-size: 16px">Helps passive and active jobseekers find better jobs. Get connected with over 45000 recruiters.
-                        Apply to jobs in just one click. Apply to thousands of jobs posted daily.</p>
-                </div>
-                <div class="panel-footer">
-                    <h3>$0</h3>
-                    <a href="jobseeker/register_user.php" style="color: inherit"><button class="btn btn-lg">Sign Up</button></a>
-                </div>
-            </div>
-        </div>
-        <div class="col-sm-4">
-            <div class="panel panel-default text-center">
-                <div class="panel-heading">
-                    <h1>Premium</h1>
-                </div>
-                <div class="panel-body">
-                    <p style="font-size: 16px;">Better Exposure <br>
-                        Better Support</p>
-                </div>
-                <div class="panel-footer">
-                    <h3>$4</h3>
-                    <h4>per month</h4>
-                    <button class="btn btn-lg disabled">Upgrade</button>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-
-
-    <div class="container bg-grey" id="contact">
-        <div class="page-header" style="background: #f4511e"></div>
-        <h2 class="text-center">CONTACT US</h2>
-        <div class="page-header"></div>
-        <div class="row">
-            <div class="col-sm-5">
-                <p>Contact us and we'll get back to you within 24 hours.</p>
-                <p><span class="glyphicon glyphicon-map-marker"></span> Adama, Ethiopia</p>
-                <p><span class="glyphicon glyphicon-phone"></span> +251953420346</p>
-                <p><span class="glyphicon glyphicon-envelope"></span>saminugus126@gmail.com</p>
-            </div>
-
-            <div class="col-sm-7">
-                <div class="row">
-                    <div class="col-sm-6 form-group">
-                        <input class="form-control" id="name" name="name" placeholder="Name" type="text" required>
-                    </div>
-                    <div class="col-sm-6 form-group">
-                        <input class="form-control" id="email" name="email" placeholder="Email" type="email" required>
-                    </div>
-                </div>
-                <textarea class="form-control" id="comments" name="comments" placeholder="Comment" rows="5"></textarea><br>
-                <div class="row">
-                    <div class="col-sm-12 form-group">
-                        <button class="btn btn-default pull-right" type="submit">Send</button>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div> <!-- Contact -->
-
-</div> <!-- sub content -->
- <div class="page-header" style="background: #f4511e"></div>
-</div> <!-- Container -->
-<!-- Set height and width with CSS -->
-<div id="googleMap" style="height:400px;width:100%;"></div>
-
-<!-- Add Google Maps -->
-<script src="http://maps.googleapis.com/maps/api/js"></script>
-<script>
-    var myCenter = new google.maps.LatLng(11.2680519,75.7891479);
-
-    function initialize() {
-        var mapProp = {
-            center:myCenter,
-            zoom:12,
-            scrollwheel:false,
-            draggable:false,
-            mapTypeId:google.maps.MapTypeId.ROADMAP
-        };
-
-        var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
-
-        var marker = new google.maps.Marker({
-            position:myCenter,
+            // Debug: Confirm search button click
+            document.querySelector('.search-btn').addEventListener('click', function(e) {
+                console.log('Search button clicked');
+            });
         });
-
-        marker.setMap(map);
-    }
-
-    google.maps.event.addDomListener(window, 'load', initialize);
-</script>
-<footer class="container-fluid text-center">
-    <a href="#insidenav" title="To Top">
-        <span class="glyphicon glyphicon-chevron-up"></span>
-    </a>
-    <p>Job Portal</p>
-</footer>
+    </script>
 </body>
-    <script src="js/jquery-1.12.0.min.js"></script>
-    <script src="js/search.js"></script>
-    <link href="css/home.css" rel="stylesheet">
-    
 </html>
